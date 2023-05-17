06e8ef8066aeb92f8582c7c1187d33991dca4820
codeforsolution <133907504+codeforsolution@users.noreply.github.com>
2023-05-17 23:34:32 +0530

    Add files via upload

---
+++ java-code/src/main/java/com/codeforsolution/java/functional/Bike.java
@@ -0,0 +1,8 @@
+package com.codeforsolution.java.functional;
+
+@FunctionalInterface
+public interface Bike {
+	
+	public void pulsar();
+
+}
---
+++ java-code/src/main/java/com/codeforsolution/java/functional/main/FunInterfaceTest.java
@@ -0,0 +1,16 @@
+package com.codeforsolution.java.functional.main;
+
+import com.codeforsolution.java.functional.impl.BikeImpl;
+
+public class FunInterfaceTest {
+
+	public static void main(String[] args) {
+			
+		//Without functional interface and lambda
+		
+		BikeImpl bike = new BikeImpl();
+		bike.pulsar();
+
+	}
+
+}
