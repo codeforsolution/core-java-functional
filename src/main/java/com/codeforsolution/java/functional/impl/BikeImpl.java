d804bdbdb28263be481d23995074c62dc9fe5ddc
codeforsolution <133907504+codeforsolution@users.noreply.github.com>
2023-05-17 23:40:51 +0530

    Add files via upload

---
+++ src/main/java/com/codeforsolution/java/functional/impl/BikeImpl.java
@@ -0,0 +1,14 @@
+package com.codeforsolution.java.functional.impl;
+
+import com.codeforsolution.java.functional.Bike;
+
+public class BikeImpl implements Bike{
+
+	@Override
+	public void pulsar() {
+		
+		System.out.println("This is pulsar method implementation");
+		
+	}
+
+}
